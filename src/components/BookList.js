b7712afa26cdcbe5e29b9cf707760169428f457d
Marcin Jarowski <jarowski.marcin@yahoo.co.uk>
2019-08-29 13:47:35 +0200

    change class components to functional components using useContext

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,30 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
 
-class BookList extends React.Component {
-  static contextType = ThemeContext;
-
-  state = {};
-
-  render() {
-    console.log(this.context);
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
-    return (
-      <div
-        className="book-list"
-        style={{ color: theme.syntax, background: theme.bg }}
-      >
-        <ul>
-          <li style={{ background: theme.ui }}>
-            Jak zdobyć przyjaciół i zjednać sobie ludzi
-          </li>
-          <li style={{ background: theme.ui }}>Lord of the rings</li>
-          <li style={{ background: theme.ui }}>Song of Ice and Fire</li>
-        </ul>
-      </div>
-    );
-  }
-}
+const BookList = () => {
+  const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const theme = isLightTheme ? light : dark;
+  return (
+    <div
+      className="book-list"
+      style={{ color: theme.syntax, background: theme.bg }}
+    >
+      <ul>
+        <li style={{ background: theme.ui }}>
+          Jak zdobyć przyjaciół i zjednać sobie ludzi
+        </li>
+        <li style={{ background: theme.ui }}>Lord of the rings</li>
+        <li style={{ background: theme.ui }}>Song of Ice and Fire</li>
+      </ul>
+    </div>
+  );
+};
 
 export default BookList;
